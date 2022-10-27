2e23ae07a2d082317927b23601213ae0a0d93efa
Ahuiting <huiting.xu@student.uni-tuebingen.de>
2022-10-27 16:54:01 +0200

    changealittlecode

--- A1_Huiting_Xu_Alessa_Straub/src/EchoFastA_Huiting_Xu_Alessa_Straub.java
+++ A1_Huiting_Xu_Alessa_Straub/src/EchoFastA_Huiting_Xu_Alessa_Straub.java
@@ -11,7 +11,7 @@ public class EchoFastA_Huiting_Xu_Alessa_Straub {
         if (args.length < 1 || args.length > 2)
             throw new IOException("Usage: EchoFastA_YOUR_NAME infile [outFile]");
         // todo: read FastA records from infile and echo to outfile or stdout (console)
-        ArrayList lst = FastA_Huiting_Xu_Alessa_Straub.read(args[0]);
+        ArrayList<FastA_Huiting_Xu_Alessa_Straub.Pair> lst = FastA_Huiting_Xu_Alessa_Straub.read(args[0]);
         String filename = (args.length > 1) ? args[1] : null;
         FastA_Huiting_Xu_Alessa_Straub.write(lst, filename);
     }
