b6055026e7f34793a22e18d5b33eeff724ff4483
Alessa <alessa.straub@web.de>
2022-11-05 14:29:06 +0100

    assignment3

--- A3_Huiting-Xu_Alessa-Straub/src/AlignmentILP_Huiting_Xu_Alessa_Straub.java
+++ A3_Huiting-Xu_Alessa-Straub/src/AlignmentILP_Huiting_Xu_Alessa_Straub.java
@@ -3,6 +3,7 @@ import assignment01.FastA_Huiting_Xu_Alessa_Straub;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 /**
  * setup ILP to solve multiple sequence alignment for three sequences
@@ -24,14 +25,31 @@ public class AlignmentILP_Huiting_Xu_Alessa_Straub {
 
 		try(var w=(args.length ==2?new FileWriter(args[1]):new OutputStreamWriter(System.out))) {
 			w.write("max: ");
-			System.out.println(list);
 			// 1. write the objective function: loop over all pairs of sequences and all pairs of letters
-			//for (int i = 0; i < list; i++) {
-				//System.out.println(list);
+			ArrayList<String> help = new ArrayList();
+			for (int i = 0; i < list.size(); i++) {
+				for (int j = i+1; j < list.size(); j++) {
+					for (int n = 0; n < list.get(i).sequence().length(); n++) {
 
-			//}
+					for (int m= 0; m < list.get(j).sequence().length(); m++) {
+						help.add((i)+String.valueOf(n)+"_"+String.valueOf(j)+String.valueOf(m));
+					}}
+				}
+
+			}
+
 
 			// 2. write out all the simple mixed cycle constraints between any two sequences
+			int count = 0;
+			for (int i = 0; i < help.size(); i++) {
+				for (int j = i+1; j < help.size(); j++) {
+					System.out.print(help.get(i)+" ");
+					count++;
+					System.out.println(help.get(j));
+				}
+
+			}
+
 
 			// 3. write out all the simple mixed cycle constraints between any three sequences
 
